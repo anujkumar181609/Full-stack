e70b367c5c12da985647ffb77c21c339fdb0cf16
Anuj Kumar <anujkumar181609@gmail.com>
2026-03-18 20:27:37 +0530

    Delete routes directory

--- routes/accountRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middleware/authmiddleware');
-const { getBalance } = require('../controllers/accountcontroller');
-
-router.get('/balance', auth, getBalance);
-
-module.exports = router;

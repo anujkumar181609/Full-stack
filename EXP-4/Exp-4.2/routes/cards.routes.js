eb40d19fed5a18094c36814169deaa332d2ca3b1
anujkumar181609 <anujkumar181609@gmail.com>
2026-03-19 07:54:16 +0530

    Added exp-5 banking API project

--- EXP-4/Exp-4.2/routes/cards.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createCard } from "../controllers/cards/createCard.controller.js";
-
-const router = express.Router();
-
-router.post("/", createCard);
-
-export default router;
-
--- EXP-4/Exp-4.3/src/modules/booking/booking.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.post("/book/:id", (req, res) => {
-    res.json({ message: "Booking successful" });
-});
-
-export default router;
